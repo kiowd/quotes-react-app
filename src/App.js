5fa4cdcbaecfbc69f76acefc16a8ff7c6081d17d
Kyaw <40131560+kiowd@users.noreply.github.com>
2020-07-08 18:25:34 +0100

    bg color changed

--- src/App.js
+++ src/App.js
@@ -4,22 +4,20 @@ import "./styles.css";
 
 export default function App() {
   const API_URL = "https://kyaw-quote-server.glitch.me/quotes/random";
-  const [hasError, setErrors] = useState(false);
   const [quotes, setQuotes] = useState({});
-  let [delay, setDelay] = useState(5000);
 
   async function fetchData() {
     const res = await fetch(API_URL);
     res
       .json()
       .then(res => setQuotes(res))
-      .catch(err => setErrors(err));
+      .catch(err => console.log(err));
   }
 
   useEffect(() => {
     const interval = setInterval(() => {
       fetchData();
-    }, delay);
+    }, 5000);
     return () => clearInterval(interval);
   }, []);
 
